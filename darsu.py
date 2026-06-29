63ffaee22b8d721dcc66c9eb9bc8db4dd6bedc1c
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-29 12:43:31 +0545

    Update darsu.py

--- darsu.py
+++ darsu.py
@@ -52,3 +52,74 @@ c = 3
 d = 8
 sum = c-d
 print(sum)
+
+#arthmetic Operators
+
+E = 5
+F = 10
+print(E + F)
+print(E - F)
+print(E * F)
+print(E / F)
+print(E % F)
+print(E ** F)
+
+#Relational Operators
+ 
+g = 30
+h = 10
+
+print(g == h)
+print(g != h)
+print(g > h)
+print(g >= h)
+print(g < h)
+print(g <= h)
+
+
+#assigment operators
+
+num = 10
+num = num + 10 
+print("num:",num)
+
+pye = 10
+pye += 10
+print("pye:", pye)
+
+
+#logical operators
+#not
+
+i = 50
+j = 30
+print(not False)
+print(not (i > j))
+
+#and
+val1 = True
+val2 = False
+print("and operator:", val1 and val2)
+ #or
+print("or operator:", val1 or val2)
+
+print("or operator:", (i == j) or (i > j))
+
+#type conversion
+k = 2
+l = 4.24
+sum = k + l
+print("type conversion:", sum )
+
+
+ab = int("5")
+ba = 4.28
+sum = ab + ba 
+print(type(a))
+print("type conversion:", sum)
+
+
+
+
+
+
